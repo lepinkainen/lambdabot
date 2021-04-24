032be994a26a99bd9d843e30c69cd6d66fe20498
Riku Lindblad <riku.lindblad@iki.fi>
2021-04-24 08:36:22 +0300

    Update tvmaze_test.go

--- command/tvmaze_test.go
+++ command/tvmaze_test.go
@@ -14,7 +14,7 @@ func TestTVMaze(t *testing.T) {
 	}{
 		//{"Mandalorean", args{args: "mandalorean"}, "Next episode of The Mandalorian 2x02 'Chapter 10: The Confrontation' airs 2020-11-06 (5 days from now) on Disney+", false},
 		{"Gilmore Girls", args{args: "gilmore girls"}, "Latest episode of Gilmore Girls 7x22 'Bon Voyage' airs 2007-05-15 (14 years ago) on The CW [Ended]", false},
-		{"The Grand Tour", args{args: "grand tour"}, "Latest episode of The Grand Tour 4x02 'The Grand Tour Presents: A Massive Hunt' airs 2020-12-17 (3 months ago) on Amazon Prime Video", false},
+		{"The Grand Tour", args{args: "grand tour"}, "Latest episode of The Grand Tour 4x02 'The Grand Tour Presents: A Massive Hunt' airs 2020-12-17 (4 months ago) on Amazon Prime Video", false},
 		{"Doctor Who", args{args: "doctor who"}, "Latest episode of Doctor Who 12x10 'The Timeless Children' airs 2020-03-01 (1 year ago) on BBC One", false},
 	}
 	for _, tt := range tests {

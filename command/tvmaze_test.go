fe3bf370104a351128367b456681a9d61ac99494
Riku Lindblad <riku.lindblad@iki.fi>
2023-07-26 09:52:59 +0300

    "Fix" the tests

--- command/tvmaze_test.go
+++ command/tvmaze_test.go
@@ -12,10 +12,10 @@ func TestTVMaze(t *testing.T) {
 		want    string
 		wantErr bool
 	}{
-		{"Obi-Wan Kenobi", args{args: "obi wan kenobi"}, "Latest episode of Obi-Wan Kenobi 1x06 'Part VI' airs 2022-06-22 (5 months ago) on Disney+ [Ended]", false},
-		{"Gilmore Girls", args{args: "gilmore girls"}, "Latest episode of Gilmore Girls 7x22 'Bon Voyage' airs 2007-05-15 (15 years ago) on The CW [Ended]", false},
-		{"The Grand Tour", args{args: "grand tour"}, "Latest episode of The Grand Tour 5x01 'The Grand Tour Presents: A Scandi Flick' airs 2022-09-16 (2 months ago) on Prime Video", false},
-		{"Doctor Who", args{args: "doctor who"}, "Latest episode of Doctor Who 13x06 'Chapter Six: The Vanquishers' airs 2021-12-05 (1 year ago) on BBC One", false},
+		{"Obi-Wan Kenobi", args{args: "obi wan kenobi"}, "Latest episode of Obi-Wan Kenobi 1x06 'Part VI' airs 2022-06-22 (1 year ago) on Disney+ [Ended]", false},
+		{"Gilmore Girls", args{args: "gilmore girls"}, "Latest episode of Gilmore Girls 7x22 'Bon Voyage' airs 2007-05-15 (16 years ago) on The CW [Ended]", false},
+		{"The Grand Tour", args{args: "grand tour"}, "Latest episode of The Grand Tour 5x02 'The Grand Tour: Eurocrash' airs 2023-06-16 (1 month ago) on Prime Video", false},
+		{"Doctor Who", args{args: "doctor who"}, "Latest episode of Doctor Who 13x06 'Chapter Six: The Vanquishers' airs 2021-12-05 (2 years ago) on BBC One", false},
 	}
 	for _, tt := range tests {
 		tt := tt

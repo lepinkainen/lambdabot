8b62e3a03cae1b5dafdb1e919169c66df90d8b86
Riku Lindblad <riku.lindblad@iki.fi>
2024-01-17 01:18:27 +0200

    Use redis as datasource for electricity prices

--- command/entsoe.go
+++ command/entsoe.go
@@ -1,12 +1,15 @@
 package command
 
 import (
+	"context"
 	"encoding/xml"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/redis/go-redis/v9"
 
 	"github.com/lepinkainen/lambdabot/lambda"
 )
@@ -67,6 +70,15 @@ type PricePoint struct {
 	Price float64
 }
 
+// GetPriceString retrieves the current, lowest, and highest prices in c/kWh from the entso-e API.
+//
+// It does this by making an HTTP request to the API, parsing the XML response, and calculating the prices.
+// The API key is retrieved from the environment variable "ENTSOE_API_KEY".
+//
+// Return:
+//   - A string containing the current, lowest, and highest prices in the format "Current: {currentPrice} c/kWh | Lowest: {lowestPrice} c/kWh | Highest: {highestPrice} c/kWh".
+//   - An error if there is an issue making the HTTP request, reading the HTTP response,
+//     unmarshaling the XML, or any other error that occurs during the process.
 func GetPriceString() (string, error) {
 
 	now := time.Now()
@@ -96,7 +108,7 @@ func GetPriceString() (string, error) {
 	}
 
 	var lowestPrice = PricePoint{Price: 999999999.0}
-	var highestPrice = PricePoint{Price: 0.0}
+	var highestPrice = PricePoint{Price: -999999999.0}
 	var currentPrice PricePoint
 
 	layout := "2006-01-02T15:04Z"
@@ -117,10 +129,12 @@ func GetPriceString() (string, error) {
 				Price: point.PriceAmount / 10 * 1.24,
 			}
 
+			// price at point is lower than lowest, set to lowest
 			if point.PriceAmount < lowestPrice.Price {
 				lowestPrice = pricePoint
-
 			}
+
+			// price at point is higher than highest, set to highest
 			if point.PriceAmount > highestPrice.Price {
 				highestPrice = pricePoint
 			}
@@ -141,6 +155,114 @@ func Entsoe(args string) (string, error) {
 	return GetPriceString()
 }
 
+var ctx = context.Background()
+
+const DBNAME = "entsoe:fi"
+
+// EntsoeRedis retrieves current, lowest, and highest prices from Redis timeseries
+//
+// # A separate system is ran in cron to update the timeseries data from entso-e
+//
+// args: A string representing the arguments for the Redis client.
+// Returns: A string containing the formatted current, lowest, and highest prices in c/kWh, and an error if any.
+func EntsoeRedis(args string) (string, error) {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_ADDR"),
+		Username: "default",
+		Password: os.Getenv("REDIS_PASSWORD"),
+		DB:       0, // use default DB
+	})
+
+	res := rdb.Ping(ctx)
+	if res.Err() != nil {
+		return "", res.Err()
+	}
+
+	var currentPrice = 999999999.0
+	var dayLow = 999999999.0
+	//var dayLowHourString = "00"
+	var dayHigh = -999999999.0
+	//var dayHighHourString = "00"
+
+	// This time we want times specifically in the Finnish time zone
+	location, _ := time.LoadLocation("Europe/Helsinki")
+
+	// current time to 1hr accuracy
+	now := time.Now().In(location).Truncate(time.Hour)
+	nowUnix := int(now.UnixMilli()) // redis likes unix milliseconds
+
+	startOfDay := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, location)
+	endOfDay := startOfDay.Add(23 * time.Hour).Add(59 * time.Minute).Add(59 * time.Second) // just before midnight
+
+	// day max
+	// ts.range entsoe:fi 1705356000000 1705442400000 AGGREGATION max 3600000000
+	// day min
+	// ts.range entsoe:fi 1705356000000 1705442400000 AGGREGATION min 3600000000
+	// NOTE: subtract 100000 from end time to get only 24 hours, otherwise you'll get 25 hours and bad data
+
+	// grab value for the current time from redis
+	valueSlice := rdb.TSRange(ctx, DBNAME, nowUnix, nowUnix)
+	if valueSlice.Err() != nil {
+		fmt.Printf("Error getting values from Redis: %+v\n", valueSlice.Err())
+		return "", valueSlice.Err()
+	}
+	tsValue, _ := valueSlice.Result()
+	if len(tsValue) > 0 {
+		currentPrice = tsValue[0].Value
+	}
+
+	// Grab maximum from redis
+	valueSlice = rdb.TSRangeWithArgs(ctx, DBNAME, int(startOfDay.UnixMilli()), int(endOfDay.UnixMilli()), &redis.TSRangeOptions{
+		Aggregator:     redis.Max,
+		BucketDuration: 3600000000,
+	})
+	if valueSlice.Err() != nil {
+		fmt.Printf("Error getting values from Redis: %+v\n", valueSlice.Err())
+		return "", valueSlice.Err()
+	}
+	tsValue, _ = valueSlice.Result()
+	if len(tsValue) > 0 {
+		dayHigh = tsValue[0].Value
+		//dayHighHourString = unixToHourString(tsValue[0].Timestamp)
+	}
+
+	// Grab minimum from redis
+	valueSlice = rdb.TSRangeWithArgs(ctx, DBNAME, int(startOfDay.UnixMilli()), int(endOfDay.UnixMilli()), &redis.TSRangeOptions{
+		Aggregator:     redis.Min,
+		BucketDuration: 3600000000,
+	})
+	if valueSlice.Err() != nil {
+		fmt.Printf("Error getting values from Redis: %+v\n", valueSlice.Err())
+		return "", valueSlice.Err()
+	}
+	tsValue, _ = valueSlice.Result()
+	if len(tsValue) > 0 {
+		dayLow = tsValue[0].Value
+		//dayLowHourString = unixToHourString(tsValue[0].Timestamp)
+	}
+
+	// TODO: dayLow and dayHigh are not working properly
+	//  Current: 11.86 c/kWh | Lowes @ 08: 10.18 c/kWh | Highest @ 08: 18.60 c/kWh
+	//
+	// Should be a range like 07-08 and 15-16
+
+	return fmt.Sprintf("Current: %.2f c/kWh | Lowest: %.2f c/kWh | Highest: %.2f c/kWh", currentPrice*1.24, dayLow*1.24, dayHigh*1.24), nil
+}
+
+func unixToHourString(unix int64) string {
+	// converting milliseconds to seconds
+	timestamp := unix / 1000
+
+	// creating a new time from Unix timestamp
+	t := time.Unix(int64(timestamp), 0)
+
+	// formatting the time to get only the hour in 24-hour format
+	hour := t.Format("15")
+
+	return hour
+}
+
 func init() {
-	lambda.RegisterHandler("sahko", Entsoe)
+	lambda.RegisterHandler("sahko", EntsoeRedis)
+	lambda.RegisterHandler("sahko2", Entsoe)
 }
--- go.mod
+++ go.mod
@@ -1,12 +1,20 @@
 module github.com/lepinkainen/lambdabot
 
-go 1.20
+go 1.21
 
 require (
-	github.com/aws/aws-lambda-go v1.41.0
+	github.com/aws/aws-lambda-go v1.44.0
 	github.com/dustin/go-humanize v1.0.1
 	github.com/pkg/errors v0.9.1
-	github.com/sirupsen/logrus v1.9.2
+	github.com/sirupsen/logrus v1.9.3
 )
 
-require golang.org/x/sys v0.8.0 // indirect
+require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+)
+
+require (
+	github.com/redis/go-redis/v9 v9.4.0
+	golang.org/x/sys v0.16.0 // indirect
+)

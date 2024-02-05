58bf3a195f32c2e939a52d4e73ad3bdd4da8cbbf
Riku Lindblad <riku.lindblad@iki.fi>
2024-02-06 00:19:48 +0200

    Add pirkka command

---
+++ command/pirkka.go
@@ -0,0 +1,43 @@
+package command
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/lepinkainen/lambdabot/lambda"
+)
+
+type Price struct {
+	Price float64 `json:"price"`
+}
+
+// Grab Pirkka price from API and respond
+func Pirkka(args string) (string, error) {
+	url := "https://juho.tech/api/pirkka_price"
+
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var price Price
+	err = json.Unmarshal(body, &price)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return fmt.Sprintf("Pirkka olut: %.2f €", price.Price), nil
+}
+
+func init() {
+	lambda.RegisterHandler("pirkka", Pirkka)
+}

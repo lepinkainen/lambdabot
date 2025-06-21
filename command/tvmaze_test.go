a18e4c8ce3a1e218ebec06fd2913aed98b64aa9c
Riku Lindblad <riku.lindblad@iki.fi>
2025-06-22 01:03:01 +0300

    Fix tests (for a while)

--- command/tvmaze_test.go
+++ command/tvmaze_test.go
@@ -12,9 +12,9 @@ func TestTVMaze(t *testing.T) {
 		want    string
 		wantErr bool
 	}{
-		{"Obi-Wan Kenobi", args{args: "obi wan kenobi"}, "Latest episode of Obi-Wan Kenobi 1x06 'Part VI' airs 2022-06-22 (2 years ago) on Disney+ [Ended]", false},
+		{"Obi-Wan Kenobi", args{args: "obi wan kenobi"}, "Latest episode of Obi-Wan Kenobi 1x06 'Part VI' airs 2022-06-22 (3 years ago) on Disney+ [Ended]", false},
 		{"Gilmore Girls", args{args: "gilmore girls"}, "Latest episode of Gilmore Girls 7x22 'Bon Voyage' airs 2007-05-15 (18 years ago) on The CW [Ended]", false},
-		{"The Grand Tour", args{args: "grand tour"}, "Latest episode of The Grand Tour 6x01 'The Grand Tour: One for the Road' airs 2024-09-13 (5 months ago) on Prime Video [Ended]", false},
+		{"The Grand Tour", args{args: "grand tour"}, "Latest episode of The Grand Tour 6x01 'The Grand Tour: One for the Road' airs 2024-09-13 (9 months ago) on Prime Video [Ended]", false},
 	}
 	for _, tt := range tests {
 		tt := tt
